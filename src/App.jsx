fcb541fc499d64c8d3b6cbb71c06993096705960
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2023-11-21 11:07:55 +0000

    commit init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import {Card} from './components/Card.jsx'
+import './App.css'
+
+function App() {
+  
+  return (
+
+<Card></Card>
+
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Card(){
+return (
+    <div className="card" >
+<img></img>
+<h2>Rodrigo Neto</h2>
+<p>Developer FullStack</p>
+        </div>
+);
+}
+
+export default Card
