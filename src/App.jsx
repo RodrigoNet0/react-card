c2f491f2947e6016b92e44b03656265a3e552884
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2023-11-21 12:07:48 +0000

    componentcard in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import {Card} from './components/Card.jsx'
-import './App.css'
+import Card from './Card.jsx'
+
 
 function App() {
   
   return (
-
 <Card></Card>
-
-  )
+  );
 }
 
 export default App
---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+
+function Card (){
+    return (
+<div className="card">
+    <img src="" alt=""></img>
+    <h2>Rodrigo Neto</h2>
+    <p>Developer FullStack</p>
+</div>
+    );
+}
