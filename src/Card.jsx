3302220f32b69aeaee55a9cca82f6fb3548e1b4a
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2023-11-23 16:36:25 +0000

    card completed

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,14 @@
+import profilePic from './assets/rodrigo.jpg'
+
 
 function Card (){
     return (
 <div className="card">
-    <img src="" alt=""></img>
-    <h2>Rodrigo Neto</h2>
-    <p>Developer FullStack</p>
+    <img src={profilePic} alt="profile picture"></img>
+    <h2 className="card-title">Rodrigo Neto</h2>
+    <p className="card-text">Developer FullStack</p>
 </div>
     );
 }
+
+export default Card
