91629a2c5fefae317074da1cd53502a3f291148f
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2023-12-04 20:58:52 +0000

    add textarea and box insert code

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,20 +1,46 @@
-import './Nav.css'
+import React, { useState } from 'react';
+import './Nav.css';
 
-function Nav (){
-    return (
-<nav>
+function Nav() {
+  const [codigoInserido, setCodigoInserido] = useState('');
+  const [mostrarInput, setMostrarInput] = useState(false);
 
-    <ul>
-        <li> FACE<i class="bi bi-2-circle-fill"></i>FACE</li>
+  const handleMostrarInput = () => {
+    setMostrarInput(true);
+  };
+
+  const handleInserirCodigo = () => {
+    // Faça algo com o código inserido, como exibi-lo em outro lugar na página ou executá-lo.
+    console.log('Código inserido:', codigoInserido);
+    // Se desejar, você pode redefinir o estado e ocultar o input novamente
+    setCodigoInserido('');
+    setMostrarInput(false);
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li> FACE<i className="bi bi-2-circle-fill"></i>FACE</li>
         <li>Actual Game</li>
         <li>About</li>
-        <li className='button1'>Insert Code</li>
+        <li className='button1' onClick={handleMostrarInput}>Insert Code</li>
+        {mostrarInput && (
+          <div className="box-text">
+            <textarea
+              value={codigoInserido}
+              onChange={(e) => setCodigoInserido(e.target.value)}
+              rows="4"
+              cols="50"
+              placeholder="Digite seu código aqui..."
+            
+            ></textarea>
+            <button className="text" onClick={handleInserirCodigo}>Inserir</button>
+          </div>
+        )}
         <li className='button2'>New Game</li>
-    </ul>
-
-</nav>
-    );
+      </ul>
+    </nav>
+  );
 }
 
-export default Nav
-
+export default Nav;
